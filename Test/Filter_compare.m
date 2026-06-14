291e98776702ea39e6c0ab0de49d609c487f3d71
Forløsning <2821611181@qq.com>
2026-06-14 20:11:20 +0800

    8基站数据集

--- Test/Filter_compare.m
+++ Test/Filter_compare.m
@@ -10,7 +10,7 @@ addpath(genpath('../Common'));
 addpath(genpath('../Filter'));
 addpath(genpath('../Data'));
 
-data_file = 'E:\SE3_MLKF\Data\Trj_data_Veh4_Anc7_3D_1.mat';
+data_file = 'E:\SE3_MLKF\Data\Trj_data_Veh4_Anc8_3D.mat';
 if ~exist(data_file, 'file')
     data_file = '../Data/Trj_data_Veh4_Anc4_3D.mat'; 
     if ~exist(data_file, 'file')
